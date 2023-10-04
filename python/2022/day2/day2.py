34329c85713d07b3462036dbc524cd8bc6dba053
Xavier Oswald <xoswald@gmail.com>
2023-10-04 19:06:08 +0200

    Add 2022 day2 second part (python)

--- python/2022/day2/day2.py
+++ python/2022/day2/day2.py
@@ -4,17 +4,25 @@ import sys
 # A, X = "Rock", "Rock"
 # B, Y = "Paper", "Paper"
 # C, Z = "Scissors", "Scissors"
+# PART2 ->
+# X means you need to lose,
+# Y means you need to end the round in a draw,
+# Z means you need to win.
 
 FILE = "input.txt"
+
 LOST = 0
 DRAW = 3
 WIN = 6
 
+ROCK = 1
+PAPER = 2
+SCISSOR = 3
+
 
 def main():
-    total = 0
-    p1 = ""
-    p2 = ""
+    total, total2 = 0, 0
+    p1, p2 = "", ""
 
     if not os.path.isfile(FILE):
         print(f'Input file "{FILE}" does not exist!')
@@ -31,14 +39,18 @@ def main():
         # Check for what you play
         if game[1] == "X":
             total = total + 1
+            total2 = total2 + LOST
             p2 = "A"
         elif game[1] == "Y":
             total = total + 2
+            total2 = total2 + DRAW
             p2 = "B"
         else:
             total = total + 3
+            total2 = total2 + WIN
             p2 = "C"
 
+        # Part 1
         # Check for DRAW / WIN / LOST
         if p1 == p2:
             total = total + DRAW
@@ -57,7 +69,34 @@ def main():
         else:
             print("UNKNOWN")
 
-    print(f"Total:{total}")
+        # Part 2
+        # End in Draw
+        if p2 == "B":
+            if p1 == "A":
+                total2 = total2 + 1
+            if p1 == "B":
+                total2 = total2 + 2
+            if p1 == "C":
+                total2 = total2 + 3
+        # End in Win
+        if p2 == "C":
+            if p1 == "A":
+                total2 = total2 + 2
+            if p1 == "B":
+                total2 = total2 + 3
+            if p1 == "C":
+                total2 = total2 + 1
+        # End in Lose
+        if p2 == "A":
+            if p1 == "A":
+                total2 = total2 + 3
+            if p1 == "B":
+                total2 = total2 + 1
+            if p1 == "C":
+                total2 = total2 + 2
+
+    print(f"Total Part1:{total}")
+    print(f"Total Part2:{total2}")
 
 
 if __name__ == "__main__":
