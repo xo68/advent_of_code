91cedbdfe9e25c77a0f5326bf85ac2174a787fe8
Xavier Oswald <xoswald@gmail.com>
2023-12-11 12:56:06 +0100

    Update day11 2023

--- python/2023/day11/aoc.py
+++ python/2023/day11/aoc.py
@@ -1,46 +1,34 @@
 if __name__ == "__main__":
     res1, res2 = 0, 0
 
-    data = []
-    lines = open("input.txt").read().split()
-    for line in lines:
-        data.append(list(line))
+    data = open("input.txt").read().split()
 
-    # Find all empty COL and ROW & Add all Positions in p
-    row = set()
-    col = set()
-    p = []
-    for r, _ in enumerate(data):
-        if data[r].count("#") == 0:
-            row.add(r)
-        for c, _ in enumerate(data[r]):
-            if data[r][c] == "#":
-                col.add(c)
-                p.append((r, c))
-    col = {i for i in range(len(data[0]))} - col
+    row = [r for r, row in enumerate(data) if all(c == "." for c in row)]
+    col = [c for c, col in enumerate(zip(*data)) if all(c == "." for c in col)]
+    p = [
+        (r, c)
+        for r, row in enumerate(data)
+        for c, col in enumerate(row)
+        if data[r][c] == "#"
+    ]
 
-    # Find all PAIRs
-    pairs = []
-    x = 0
-    for p1 in range(len(p)):
-        for p2 in range(1 + x, len(p)):
-            pairs.append((p[p1], p[p2]))
-        x = x + 1
+    pairs = [(p[p1], p[p2]) for p1 in range(len(p)) for p2 in range(p1 + 1, len(p))]
 
-    # Compute Part 1 & 2
     for p in pairs:
-        start, end = p[0], p[1]
         empty_r, empty_c = 0, 0
         for r in row:
-            if start[0] < r < end[0]:
+            if p[0][0] < r < p[1][0]:
                 empty_r += 1
         for c in col:
-            if min(start[1], end[1]) < c < max(start[1], end[1]):
+            if min(p[0][1], p[1][1]) < c < max(p[0][1], p[1][1]):
                 empty_c += 1
-        r = abs(start[0] - end[0])
-        c = abs(start[1] - end[1])
-        res1 += r + empty_r + c + empty_c
-        res2 += r + empty_r * int(1e6 - 1) + c + empty_c * int(1e6 - 1)
+        res1 += abs(p[0][0] - p[1][0]) + empty_r + abs(p[0][1] - p[1][1]) + empty_c
+        res2 += (
+            abs(p[0][0] - p[1][0])
+            + empty_r * int(1e6 - 1)
+            + abs(p[0][1] - p[1][1])
+            + empty_c * int(1e6 - 1)
+        )
 
     print(f"Result1:{res1}")
     print(f"Result2:{res2}")
