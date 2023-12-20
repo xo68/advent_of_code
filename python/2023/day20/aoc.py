bd17889c0301058d9c6499f46bc211907a546416
Xavier Oswald <xoswald@gmail.com>
2023-12-20 23:50:43 +0100

    Update 2023 day20 - Part2

--- python/2023/day20/aoc.py
+++ python/2023/day20/aoc.py
@@ -1,3 +1,5 @@
+import math
+
 if __name__ == "__main__":
     res1, res2 = 0, 0
     LOW, HIGH = 1, 2
@@ -24,6 +26,7 @@ if __name__ == "__main__":
                 if e1 in evt[e2][2]:
                     evt[e1][3][e2] = LOW
 
+    # Part 1
     LP, HP = 0, 0
     for _ in range(1000):
         LP += 1
@@ -78,7 +81,70 @@ if __name__ == "__main__":
                             signal = HIGH
 
                         next.append((name, signal, evt[name][2]))
-
     res1 = LP * HP
     print(f"Result1:{res1}")
-    print(f"Result2:{res2}")
+
+    # Part 2 - HUGLY COPY OF PART 1 :(
+    cl = {}
+    seen = {"vt": 0, "xr": 0, "fv": 0, "kk": 0}
+
+    push = 0
+    while True:
+        push += 1
+        signal = LOW
+        next = [("broadcaster", LOW, evt["broadcaster"][2])]
+        while next:
+            dst = next[:]
+            next = []
+
+            for i, d in enumerate(dst):
+                src = dst[i][0]
+                sig = dst[i][1]
+
+                for name in dst[i][2]:
+                    if name not in evt.keys():
+                        continue
+                    signal = sig
+
+                    if name == "sq" and signal == HIGH:
+                        seen[src] += 1
+
+                        if src not in cl:
+                            cl[src] = push
+
+                        if all(seen.values()):
+                            res2 = math.lcm(*cl.values())
+                            print(f"Result2:{res2}")
+                            exit(1)
+                    # 0: Type; 1: On/Off; 2: list of destinations
+                    # Flip / Flop -> High pulse, nothing happen
+                    if evt[name][0] == "%":
+                        if signal == LOW:
+                            if evt[name][1] is False:
+                                evt[name][1] = True
+                                signal = HIGH
+                                # From , signal, targets
+                            else:
+                                evt[name][1] = False
+                                signal = LOW
+                            next.append((name, signal, evt[name][2]))
+
+                    # Conjunction
+                    if evt[name][0] == "&":
+                        # Store the current signal
+                        if signal == LOW:
+                            evt[name][3][src] = LOW
+                        else:
+                            evt[name][3][src] = HIGH
+
+                        pulse = HIGH
+                        for p in evt[name][3]:
+                            if evt[name][3][p] == LOW:
+                                pulse = LOW
+                                break
+                        if pulse == HIGH:
+                            signal = LOW
+                        else:
+                            signal = HIGH
+
+                        next.append((name, signal, evt[name][2]))
